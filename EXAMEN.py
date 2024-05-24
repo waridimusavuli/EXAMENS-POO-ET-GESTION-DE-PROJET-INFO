a79cbd1111c46114c104ec306ef55e22a1664de1
Waridi Musavuli <waridimusavuli@gmail.com>
2024-05-24 15:15:06 +0200

    creation de la classe calculer qui erite de la class notes et creation de la boucle while

--- EXAMEN.py
+++ EXAMEN.py
@@ -12,13 +12,4 @@ class calcul(notes):
             note = int(input(f"taper la note numero {i+1}: "))
             notes.append(note)
             i=i+1
-        print(notes)
-        maximum=max(notes)
-        minimum=min(notes)
-        for i in range(len(notes)):
-            somme=somme+notes[i]
-        moyenne=somme/9
-        resultat = f"parmis le nombre que tu as entre le plus grand est {maximum}, et le minum {minimum} et la moyenne {moyenne} "
-        print(resultat)
-calcul1 = calcul()
-calcul1.somme()
+       
